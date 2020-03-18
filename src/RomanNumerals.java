ea8fa506375ea44fd8ea621484129527899b6c49
valriats <valria.ts@yandex.ru>
2020-03-19 02:21:24 +0300

    Init project. Add task about roman numerals

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class RomanNumerals {
 
@@ -21,28 +18,39 @@ public class RomanNumerals {
 
     public static void main(String... args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        boolean stop  = false;
-        while (!stop) {
+        while (true) {
             String input = reader.readLine();
-            if(input.equals("stop")) stop = true;
+            if (input.equals("stop")) return;
+            if (!input.matches("\\d+")) {
+                System.out.println("We wait only numbers");
+                continue;
+            }
+            if (input.startsWith("0")) {
+                System.out.println("We wait only numbers not starting from zero");
+                continue;
+            }
+            if (Long.parseLong(input) > 5000) {
+                System.out.println("We can parse only numbers less 5000");
+                continue;
+            }
 
             List<Integer> listNumbers = new ArrayList<>();
 
-            String[] simbols = input.split("");
+            String[] symbols = input.split("");
             for (int i = 0; i < input.length(); i++) {
-                StringBuilder number = new StringBuilder(simbols[i]);
+                StringBuilder number = new StringBuilder(symbols[i]);
 
                 int countOfZeros = input.length() - 1 - i;
 
                 number.append("0".repeat(countOfZeros));
-                if(number.toString().equals("0")) continue;
+                if (number.toString().equals("0")) continue;
                 listNumbers.add(Integer.valueOf(number.toString()));
             }
 
             StringBuilder result = new StringBuilder();
 
             listNumbers.forEach(n -> {
-               getSymbolOfNumber(n, result);
+                getSymbolOfNumber(n, result);
             });
 
             System.out.println(result.toString());
@@ -51,7 +59,7 @@ public class RomanNumerals {
 
     private static void getSymbolOfNumber(Integer num, StringBuilder result) {
         List<Integer> numbers = List.of(1, 5, 10, 50, 100, 500, 1000, 5000);
-        if(numbers.contains(num)) {
+        if (numbers.contains(num)) {
             result.append(numbersInRoman.get(num));
             return;
         }
@@ -61,8 +69,8 @@ public class RomanNumerals {
 
         int countOfZeroes = num.toString().length() - 1;
 
-        for (int i = 0; i<=numbers.size() - 1; i++) {
-            if(!(num > numbers.get(i))) {
+        for (int i = 0; i <= numbers.size() - 1; i++) {
+            if (!(num > numbers.get(i))) {
                 prev = numbers.get(i - 1);
                 next = numbers.get(i);
                 break;
@@ -70,8 +78,8 @@ public class RomanNumerals {
         }
 
         int difference;
-        if((num - prev) / Integer.valueOf("1" + "0".repeat(countOfZeroes)) >= 4
-                ||  (num / Integer.valueOf("1" + "0".repeat(countOfZeroes)) == 4)) {
+        if ((num - prev) / Integer.parseInt("1" + "0".repeat(countOfZeroes)) >= 4
+                || (num / Integer.parseInt("1" + "0".repeat(countOfZeroes)) == 4)) {
             difference = next - num;
             getSymbolOfNumber(difference, result);
             result.append(numbersInRoman.get(next));
@@ -82,3 +90,4 @@ public class RomanNumerals {
         }
     }
 }
+
